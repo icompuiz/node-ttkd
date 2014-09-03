f4f2fdc051267f8630c5c07aa4753140bec33678
njfuschino <njfuschino@gmail.com>
2014-09-03 19:58:29 -0400

    Implement add ranks and include references to/from programsn and classes/ranks

--- app/scripts/controllers/programs/programs.js
+++ app/scripts/controllers/programs/programs.js
@@ -3,12 +3,15 @@ define(['../module'], function(controllers){
 	controllers.controller('ProgramsCtrl', ['$scope', '$state', 'Restangular', function($scope, $state, Restangular) {
 		var basePrograms = Restangular.all('programs');
 		var baseClasses = Restangular.all('classes');
+		var baseRanks = Restangular.all('ranks');
 
 		$scope.programs = {};
 		$scope.newProgram = {
-			classes: []
+			classes: [],
+			ranks: []
 		};
 		$scope.newClass = {};
+		$scope.newRank = {};
 
 		$scope.getPrograms = function() {
 			$scope.programs = basePrograms.getList().$object;
@@ -29,37 +32,61 @@ define(['../module'], function(controllers){
 			};
 			//POST new program
 			basePrograms.post(programToAdd).then(function(programAdded) {
-				var classIDs = []; //Holds reference to program's classes
-				async.each($scope.newProgram.classes,
-					function(classItem, callback) {
-						var classToAdd = {
-							name: classItem.name,
-							program: programAdded._id
-						};
-
-						//POST each new class and add object ID to array
-						baseClasses.post(classToAdd).then(function(classAdded, err){
-							classIDs.push(classAdded._id);							
-							callback(err);
-						});
-					}, 
-					function(err){
-						//Add class references and PUT updated program
+				var classIDs = [],
+					rankIDs = [];
+				//Add classes to db
+				async.parallel([
+					function(callback, err) {
+						//Add classes
+						var i = 0;
+						for (i=0; i<$scope.newProgram.classes.length; i++) {
+							var classToAdd = {
+								name: $scope.newProgram.classes.name,
+								program: programAdded._id
+							};
+							//POST each new class and add object ID to array
+							baseClasses.post(classToAdd).then(function(classAdded, err){
+								classIDs.push(classAdded._id);
+							});
+						}
+						callback();
+					},
+					function(callback, err) {
+						//add Ranks
+						var i = 0;
+						for(i=0; i<$scope.newProgram.ranks.length; i++) {
+							var rankToAdd = {
+								name: $scope.newProgram.ranks.name,
+								program: programAdded._id
+							};
+							//POST each new rank and add object ID to array
+							baseRanks.post(rankToAdd).then(function(rankAdded, err){
+								rankIDs.push(rankAdded._id);
+							});
+						}
+						callback();
+					}],
+					function(err) {
+						//Add class and rank references and PUT updated program
 						Restangular.one('programs', programAdded._id).get().then(function(p){
 							var programToUpdate = p;
 							programToUpdate.classes = classIDs;
-							console.log(p);
+							programToUpdate.ranks = rankIDs;
 							programToUpdate.put();
 						});
 						$scope.newProgram = {};
-					}
-				);
+				});
 			});
 		};
 
 		$scope.addClass = function() {
 			$scope.newProgram.classes.push($scope.newClass);
 			$scope.newClass = {};
+		};
+
+		$scope.addRank = function() {
+			$scope.newProgram.ranks.push($scope.newRank);
+			$scope.newRank = {};
 		};
 	}]);
 });	
